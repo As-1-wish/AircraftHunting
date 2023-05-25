eb15689d2b894863d567e9261b9d535afe10036b
As-1-wish <117714807+As-1-wish@users.noreply.github.com>
2023-05-25 14:41:56 +0800

    Delete pursuit2 directory

--- pursuit2/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Pursuit2Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pursuit2'
--- pursuit2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from pursuit2.views import Evaluate
-
-app_name = "pursuit2"
-
-urlpatterns = [
-    path('evaluate', Evaluate, name='evaluate'),
-]
